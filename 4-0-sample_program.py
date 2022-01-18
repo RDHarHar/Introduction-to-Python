7640afe8b80204faedfa69a6b5728a9214d7df5f
Ryan Harwick <ryandharwick@gmail.com>
2022-01-18 18:55:18 -0500

    Week1

---
+++ 4-0-sample_program.py
@@ -0,0 +1,13 @@
+# Bryant Moscon
+# August 15, 2020
+# 
+# This is my sample program, with a block comment at the top. The block comment needs to contain the following
+# 1. Name
+# 2. Date
+# 3. Program description
+
+favorite_number = 4
+print("My favorite number is:", favorite_number)
+print("My favorite number times 2 is:", favorite_number * 2)
+print("My favorite number cubed is:", favorite_number ** 3)
+print("Whats your favorite number?")
