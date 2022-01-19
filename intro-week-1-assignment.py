62128ba4343cbcee5bbb0018db1b4f3a83ff22b7
Ryan Harwick <ryandharwick@gmail.com>
2022-01-18 19:19:48 -0500

    Week 1 assignment

---
+++ intro-week-1-assignment.py
@@ -0,0 +1,8 @@
+# 1. Ryan Harwick
+# 2. 2022-01-18
+# 3. Week 1 - Assignment
+
+x = 4
+ans = (4*x+3)/0.2
+
+print(f"Hello, welcome to my pthon program! My name is Ryan.\nThe value of x is {x}. The result of (4x + 3)/0.2 is {ans}")
